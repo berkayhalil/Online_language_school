41b83aabc125727c925cee0d805bc50cbbb5e225
berkayhalil <100590546+berkayhalil@users.noreply.github.com>
2024-11-04 17:32:20 +0100

    change

---
+++ script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function collapseMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className.includes("responsive")) {
+        x.className = "topnav"; // Collapse menu
+    }
+}
+
+
+
+window.addEventListener('resize', function() {
+    var x = document.getElementById("myTopnav");
+    if (window.innerWidth > 600) {
+        x.className = "topnav"; // Collapse the menu if screen width is greater than 600px
+    }
+});
